55b320ba5b8fab4d1d5c3ff70b557e73bea8ba39
Pavel Kalachev <pkalachev@usetech.com>
2021-12-06 09:48:40 +0300

    fix: color negative to coral

--- src/stories/Colors.stories.tsx
+++ src/stories/Colors.stories.tsx
@@ -21,9 +21,9 @@ Base.args = {
         'color-secondary-500',
         'color-blue-grey-500',
         'color-grey-500',
+        'color-coral-500',
         'color-additional-positive-500',
-        'color-additional-warning-500',
-        'color-additional-negative-500'
+        'color-additional-warning-500'
     ]
 };
 
@@ -63,12 +63,17 @@ Palette.args = {
         'color-grey-500',
         'color-grey-600',
         'color-grey-700',
+        'color-coral-100',
+        'color-coral-200',
+        'color-coral-300',
+        'color-coral-400',
+        'color-coral-500',
+        'color-coral-600',
+        'color-coral-700',
         'color-additional-positive-500',
         'color-additional-positive-100',
         'color-additional-warning-500',
-        'color-additional-warning-100',
-        'color-additional-negative-500',
-        'color-additional-negative-100'
+        'color-additional-warning-100'
     ]
 };
 
