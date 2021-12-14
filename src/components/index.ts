2d7a00d08f98e439baed38878949ad6b9985b797
Pavel Kalachev <pkalachev@usetech.com>
2021-12-14 17:20:42 +0300

    feat: RadioGroup add

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,7 @@ export { default as ColorPalette } from './ColorPalette/ColorPalette';
 export { default as Icon } from './Icon/Icon';
 export { default as InputText } from './InputText/InputText';
 export { default as Layout } from './Layout/Layout';
+export { default as RadioGroup } from './RadioGroup/RadioGroup';
 export { default as Select } from './Select/Select';
 export { default as Tabs } from './Tabs/Tabs';
 export { default as Toggle } from './Toggle/Toggle';
