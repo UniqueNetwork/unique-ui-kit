cd793d894946c445ac816b00c69739fddf54b796
Анна Михайлова <annamihailova@MacBook-Pro-Anna.local>
2021-12-23 10:22:51 +0300

    fix: sorting works

--- src/components/Table/TableOuter.tsx
+++ src/components/Table/TableOuter.tsx
@@ -17,7 +17,7 @@ import { Table } from '..';
 import { Text, Icon } from '../../components';
 
 interface ITableOuterProps {
-    columns?: [
+    columns: [
         {
             title: string;
             dataIndex: string;
@@ -30,165 +30,165 @@ interface ITableOuterProps {
             columnClickHandle: (column: string) => {};
         }
     ];
-    data?: [];
+    data: [];
     className?: string;
 }
-const resultFromAction = {
-    Burn: 'Token destroyed',
-    Transfer: 'Transferred to',
-    Mint: 'Token created'
-};
+// const resultFromAction = {
+//     Burn: 'Token destroyed',
+//     Transfer: 'Transferred to',
+//     Mint: 'Token created'
+// };
 
-const iconFromAction = {
-    Burn: 'Token destroyed',
-    Transfer: 'Transferred to',
-    Mint: 'Token created'
-};
+// const iconFromAction = {
+//     Burn: 'Token destroyed',
+//     Transfer: 'Transferred to',
+//     Mint: 'Token created'
+// };
 
-let dataArray = [
-    {
-        action: 'Burn',
-        time: '15-09-2021, 13:50:29',
-        fee: '0.0001 QTZ',
-        author: '14KBS…trcQH',
-        result: 'Token destroyed'
-    },
-    {
-        action: 'Transfer',
-        time: '15-09-2021, 13:50:29',
-        fee: '0.0001 QTZ',
-        author: '14KBS…trcQH',
-        result: '14KBS…trcQH'
-    },
-    {
-        action: 'Transfer',
-        time: '15-09-2021, 13:50:29',
-        fee: '0.0001 QTZ',
-        author: '14KBS…trcQH',
-        result: '14KBS…trcQH'
-    },
-    {
-        action: 'Transfer',
-        time: '15-09-2021, 13:50:29',
-        fee: '0.0001 QTZ',
-        author: '14KBS…trcQH',
-        result: 'Transfer failed'
-    },
-    {
-        action: 'Transfer',
-        time: '15-09-2021, 13:50:29',
-        fee: '0.0001 QTZ',
-        author: '14KBS…trcQH',
-        result: '14KBS…trcQH'
-    },
-    {
-        action: 'Mint',
-        time: '15-09-2021, 13:50:29',
-        fee: '0.0001 QTZ',
-        author: '14KBS…trcQH',
-        result: 'Token created'
-    }
-];
-const sorting = (column:string) => {
-    dataArray = dataArray.sort((a,b)=>{
-        console.log('sort by column', column);
-        console.log('a[column]', a[column]);
-        console.log('b[column]', b[column]);
-        if(a[column]>b[column]){
-            console.log('1');
-            return 1;
-        } else if(a[column]<b[column]){
-            console.log('2');
-            return -1;
-        } else {
-            console.log('3');
-            return 0;
-        }
+// let dataArray = [
+//     {
+//         action: 'Burn',
+//         time: '15-09-2021, 13:50:29',
+//         fee: '0.0001 QTZ',
+//         author: '14KBS…trcQH',
+//         result: 'Token destroyed'
+//     },
+//     {
+//         action: 'Transfer',
+//         time: '15-09-2021, 13:50:29',
+//         fee: '0.0001 QTZ',
+//         author: '14KBS…trcQH',
+//         result: '14KBS…trcQH'
+//     },
+//     {
+//         action: 'Transfer',
+//         time: '15-09-2021, 13:50:29',
+//         fee: '0.0001 QTZ',
+//         author: '14KBS…trcQH',
+//         result: '14KBS…trcQH'
+//     },
+//     {
+//         action: 'Transfer',
+//         time: '15-09-2021, 13:50:29',
+//         fee: '0.0001 QTZ',
+//         author: '14KBS…trcQH',
+//         result: 'Transfer failed'
+//     },
+//     {
+//         action: 'Transfer',
+//         time: '15-09-2021, 13:50:29',
+//         fee: '0.0001 QTZ',
+//         author: '14KBS…trcQH',
+//         result: '14KBS…trcQH'
+//     },
+//     {
+//         action: 'Mint',
+//         time: '15-09-2021, 13:50:29',
+//         fee: '0.0001 QTZ',
+//         author: '14KBS…trcQH',
+//         result: 'Token created'
+//     }
+// ];
+// const sorting = (column:string) => {
+//     dataArray = dataArray.sort((a,b)=>{
+//         console.log('sort by column', column);
+//         console.log('a[column]', a[column]);
+//         console.log('b[column]', b[column]);
+//         if(a[column]>b[column]){
+//             console.log('1');
+//             return 1;
+//         } else if(a[column]<b[column]){
+//             console.log('2');
+//             return -1;
+//         } else {
+//             console.log('3');
+//             return 0;
+//         }
 
-        console.log('dataArray', dataArray);
-})};
-const clicking = (column:string) => {
-    console.log('click on column', column);
-    console.log('dataArray', dataArray);
-};
-const columnsArray = [
-    {
-        title: 'Action',
-        dataIndex: 'action',
-        key: 'action',
-        width: 100,
-        size: 'm',
-        color: 'blue-grey',
-        icon: 'arrows-down-up',
-        render: (rowNumber:number) => (
-            <Text size="m" color="additional-dark">
-                {dataArray[rowNumber].action}
-            </Text>
-        ),
-        columnClickHandle: sorting
-    },
-    {
-        title: 'Time',
-        dataIndex: 'time',
-        key: 'time',
-        width: 100,
-        size: 'm',
-        color: 'primary',
-        icon: 'clock',
-        render: (rowNumber:number) => (
-            <Text size="m" color="additional-dark">
-                {dataArray[rowNumber].time}
-            </Text>
-        ),
-        columnClickHandle: clicking
-    },
-    {
-        title: 'Fee',
-        dataIndex: 'fee',
-        key: 'fee',
-        width: 200,
-        icon: 'arrows-down-up',
-        render: (rowNumber:number) => (
-            <Text size="m" color="additional-dark">
-                {dataArray[rowNumber].fee}
-            </Text>
-        ),
-        columnClickHandle: sorting
-    },
-    {
-        title: 'Author',
-        dataIndex: 'author',
-        key: 'author',
-        width: 200,
-        render: (rowNumber:number) => (
-            <Text size="m" color="primary-500">
-                {dataArray[rowNumber].author}
-            </Text>
-        )
-    },
-    {
-        title: 'Result',
-        dataIndex: 'result',
-        key: 'result',
-        render: (rowNumber:number) => (
-            <>
-                <Text size="m" color='blue-grey-600'>
-                    {resultFromAction[dataArray[rowNumber].action]}
-                </Text>
-                &nbsp;
-                <Text size="m" color="primary-500">
-                    {dataArray[rowNumber].action === 'Transfer'
-                        ? dataArray[rowNumber].result
-                        : ''}
-                </Text>
-            </>
-        )
-    }
-];
+//         console.log('dataArray', dataArray);
+// })};
+// const clicking = (column:string) => {
+//     console.log('click on column', column);
+//     console.log('dataArray', dataArray);
+// };
+// const columnsArray = [
+//     {
+//         title: 'Action',
+//         dataIndex: 'action',
+//         key: 'action',
+//         width: 100,
+//         size: 'm',
+//         color: 'blue-grey',
+//         icon: 'arrows-down-up',
+//         render: (rowNumber:number) => (
+//             <Text size="m" color="additional-dark">
+//                 {dataArray[rowNumber].action}
+//             </Text>
+//         ),
+//         columnClickHandle: sorting
+//     },
+//     {
+//         title: 'Time',
+//         dataIndex: 'time',
+//         key: 'time',
+//         width: 100,
+//         size: 'm',
+//         color: 'primary',
+//         icon: 'clock',
+//         render: (rowNumber:number) => (
+//             <Text size="m" color="additional-dark">
+//                 {dataArray[rowNumber].time}
+//             </Text>
+//         ),
+//         columnClickHandle: clicking
+//     },
+//     {
+//         title: 'Fee',
+//         dataIndex: 'fee',
+//         key: 'fee',
+//         width: 200,
+//         icon: 'arrows-down-up',
+//         render: (rowNumber:number) => (
+//             <Text size="m" color="additional-dark">
+//                 {dataArray[rowNumber].fee}
+//             </Text>
+//         ),
+//         columnClickHandle: sorting
+//     },
+//     {
+//         title: 'Author',
+//         dataIndex: 'author',
+//         key: 'author',
+//         width: 200,
+//         render: (rowNumber:number) => (
+//             <Text size="m" color="primary-500">
+//                 {dataArray[rowNumber].author}
+//             </Text>
+//         )
+//     },
+//     {
+//         title: 'Result',
+//         dataIndex: 'result',
+//         key: 'result',
+//         render: (rowNumber:number) => (
+//             <>
+//                 <Text size="m" color='blue-grey-600'>
+//                     {resultFromAction[dataArray[rowNumber].action]}
+//                 </Text>
+//                 &nbsp;
+//                 <Text size="m" color="primary-500">
+//                     {dataArray[rowNumber].action === 'Transfer'
+//                         ? dataArray[rowNumber].result
+//                         : ''}
+//                 </Text>
+//             </>
+//         )
+//     }
+// ];
 
 const TableOuter: FC<ITableOuterProps> = ({
-    columns = [...columnsArray],
-    data = [...dataArray],
+    columns,
+    data,
     className
 }: ITableOuterProps) => {
 
--- src/stories/Table.stories.tsx
+++ src/stories/Table.stories.tsx
@@ -1,58 +1,169 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Table } from '../components';
-import {
-    TableBody,
-    TableHead,
-    TableHeaderCell,
-    TableRow,
-    TableCell,
-    DashedDivider
-} from '../components/Table/components';
+import { Table, Text } from '../components';
 import TableOuter from '../components/Table/TableOuter';
+import { useState } from 'react';
 
 export default {
     title: 'Components/Table',
     component: Table
 } as ComponentMeta<typeof Table>;
 
-const Template: ComponentStory<typeof Table> = (args) => (
-    <>
-    <Table {...args}>
-        {/* <TableHead>Hello</TableHead> */}
-        <TableBody>
-            <TableRow>
-                <TableHeaderCell>Extrinsic</TableHeaderCell>
-                <TableHeaderCell>Age</TableHeaderCell>
-                <TableHeaderCell>From</TableHeaderCell>
-                <TableHeaderCell>To</TableHeaderCell>
-                <TableHeaderCell>Amount</TableHeaderCell>
-            </TableRow>
-            <TableRow>
-                <TableCell>a</TableCell>
-                <TableCell>bbbbbbbbbbbbbbbbbbbbbbbb</TableCell>
-                <TableCell>c</TableCell>
-                <TableCell>b</TableCell>
-                <TableCell>c</TableCell>
-            </TableRow>
+let dataArray = [
+    {
+        action: 'Burn',
+        time: '15-09-2021, 13:50:29',
+        fee: '0.0001 QTZ',
+        author: '14KBS…trcQH',
+        result: 'Token destroyed'
+    },
+    {
+        action: 'Transfer',
+        time: '15-09-2021, 13:50:29',
+        fee: '0.0001 QTZ',
+        author: '14KBS…trcQH',
+        result: '14KBS…trcQH'
+    },
+    {
+        action: 'Transfer',
+        time: '15-09-2021, 13:50:29',
+        fee: '0.0001 QTZ',
+        author: '14KBS…trcQH',
+        result: '14KBS…trcQH'
+    },
+    {
+        action: 'Transfer',
+        time: '15-09-2021, 13:50:29',
+        fee: '0.0001 QTZ',
+        author: '14KBS…trcQH',
+        result: 'Transfer failed'
+    },
+    {
+        action: 'Transfer',
+        time: '15-09-2021, 13:50:29',
+        fee: '0.0001 QTZ',
+        author: '14KBS…trcQH',
+        result: '14KBS…trcQH'
+    },
+    {
+        action: 'Mint',
+        time: '15-09-2021, 13:50:29',
+        fee: '0.0001 QTZ',
+        author: '14KBS…trcQH',
+        result: 'Token created'
+    }
+];
 
-            <DashedDivider/>
 
-            <TableRow>
-                <TableCell>d</TableCell>
-                <TableCell>e</TableCell>
-                <TableCell>f</TableCell>
-                <TableCell>b</TableCell>
-                <TableCell>c</TableCell>
-            </TableRow>
-            <DashedDivider/>
-        </TableBody>
-    </Table>
-    <TableOuter></TableOuter>
-    </>
-);
+const Template: ComponentStory<typeof TableOuter> = (args) => {
+    const [tableData, setTableData] = useState({...args.data});
+    const sorting = (column: string) => {
+        let sortingTableData = args.data.sort((a, b) => {
+            console.log('sort by column', column);
+            console.log('a[column]', a[column]);
+            console.log('b[column]', b[column]);
+            if (a[column] > b[column]) {
+                console.log('1');
+                return 1;
+            } else if (a[column] < b[column]) {
+                console.log('2');
+                return -1;
+            } else {
+                console.log('3');
+                return 0;
+            }
+        });
+        console.log('setTableData');
+        setTableData(sortingTableData);
+    };
+    const clicking = (column: string) => {
+        console.log('click on column', column);
+        console.log('dataArray', dataArray);
+    };
+    const resultFromAction = {
+        Burn: 'Token destroyed',
+        Transfer: 'Transferred to',
+        Mint: 'Token created'
+    };
+    const columnsArray = [
+        {
+            title: 'Action',
+            dataIndex: 'action',
+            key: 'action',
+            width: 100,
+            size: 'm',
+            color: 'blue-grey',
+            icon: 'arrows-down-up',
+            render: (rowNumber: number) => (
+                <Text size="m" color="additional-dark">
+                    {dataArray[rowNumber].action}
+                </Text>
+            ),
+            columnClickHandle: sorting
+        },
+        {
+            title: 'Time',
+            dataIndex: 'time',
+            key: 'time',
+            width: 100,
+            size: 'm',
+            color: 'primary',
+            icon: 'clock',
+            render: (rowNumber: number) => (
+                <Text size="m" color="additional-dark">
+                    {dataArray[rowNumber].time}
+                </Text>
+            ),
+            columnClickHandle: clicking
+        },
+        {
+            title: 'Fee',
+            dataIndex: 'fee',
+            key: 'fee',
+            width: 200,
+            icon: 'arrows-down-up',
+            render: (rowNumber: number) => (
+                <Text size="m" color="additional-dark">
+                    {dataArray[rowNumber].fee}
+                </Text>
+            ),
+            columnClickHandle: sorting
+        },
+        {
+            title: 'Author',
+            dataIndex: 'author',
+            key: 'author',
+            width: 200,
+            render: (rowNumber: number) => (
+                <Text size="m" color="primary-500">
+                    {dataArray[rowNumber].author}
+                </Text>
+            )
+        },
+        {
+            title: 'Result',
+            dataIndex: 'result',
+            key: 'result',
+            render: (rowNumber: number) => (
+                <>
+                    <Text size="m" color="blue-grey-600">
+                        {resultFromAction[dataArray[rowNumber].action]}
+                    </Text>
+                    &nbsp;
+                    <Text size="m" color="primary-500">
+                        {dataArray[rowNumber].action === 'Transfer'
+                            ? dataArray[rowNumber].result
+                            : ''}
+                    </Text>
+                </>
+            )
+        }
+    ];
+    return <TableOuter {...args} columns={columnsArray }></TableOuter>;
+};
 
 export const DefaultH1 = Template.bind({});
-
-DefaultH1.args = {};
+DefaultH1.args = {
+    data: dataArray
+};
 
 DefaultH1.storyName = 'Default H1';
