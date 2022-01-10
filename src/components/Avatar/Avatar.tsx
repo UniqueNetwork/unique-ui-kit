4b58502270c34ec2515bb441870af987678952a6
a-mikhailova <amikhailova@usetech.com>
2022-01-10 09:30:43 +0300

    fix:types

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,38 +2,39 @@
  * @author Anna Mikhailova <amikhailova@usetech.com>
  */
 
-import { FC } from 'react';
-import './Avatar.scss';
-import classNames from 'classnames';
-
-interface IComponentProps {
-    src: string;
-    className: string;
-    size: number;
-    type: 'circle' | 'square';
-}
-
-const Avatar: FC<IComponentProps> = ({
-    src,
-    className,
-    size,
-    type = 'square'
-}: IComponentProps) => {
-    return (
-        <img
-            className={classNames(
-                'unique-avatar',
-                {
-                    circle: type === 'circle',
-                    square: type === 'square'
-                },
-                className
-            )}
-            alt="avatar"
-            src={src}
-            width={size}
-        />
-    );
-};
-
-export default Avatar;
+ import { FC } from 'react';
+ import './Avatar.scss';
+ import classNames from 'classnames';
+ 
+ export interface IAvatarProps {
+     src: string;
+     size: number;
+     type?: 'circle' | 'square';
+     className?: string;
+ }
+ 
+ const Avatar: FC<IAvatarProps> = ({
+     src,
+     className,
+     size,
+     type = 'square'
+ }: IAvatarProps) => {
+     return (
+         <img
+             className={classNames(
+                 'unique-avatar',
+                 {
+                     circle: type === 'circle',
+                     square: type === 'square'
+                 },
+                 className
+             )}
+             alt="avatar"
+             src={src}
+             width={size}
+         />
+     );
+ };
+ 
+ export default Avatar;
+ 
