ad8b9542168a0f155bb0b27284ee31038402e87c
Roman Beganov <rbeganov@usetech.com>
2022-02-03 09:40:46 +0500

    test(button): remove useless tests

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -2,26 +2,6 @@ import { fireEvent, getByText, render, screen } from '@testing-library/react';
 import React from 'react';
 import Button from './Button';
 
-it('text check', () => {
-    const options = {
-        title: 'button text',
-        onClick: () => {
-            console.log('button click');
-        }
-    };
-    render(<Button {...options} />);
-    expect(screen.queryByText(options.title)).toBeTruthy();
-});
-
-it('click check', () => {
-    const onClick = jest.fn();
-    const { container } = render(
-        <Button onClick={onClick} title="button text" />
-    );
-    fireEvent.click(getByText(container, 'button text'));
-    expect(onClick).toHaveBeenCalled();
-});
-
 it('role check', () => {
     const { container } = render(
         <Button onClick={() => {}} title="button text" role="tertiary" />
@@ -58,16 +38,4 @@ it('wide check', () => {
     expect(
         getByText(container, 'button text').classList.contains('wide')
     ).toBeTruthy();
-});
-
-it('disabled check', () => {
-    const options = {
-        title: 'button text',
-        disabled: true,
-        onClick: () => {
-            console.log('button click');
-        }
-    };
-    render(<Button {...options} />);
-    expect(screen.queryByRole('button')).toHaveProperty('disabled');
 });
