de051678a6bd3eb96f5f88f07ebc736485fc52a4
Roman Beganov <rbeganov@usetech.com>
2022-02-03 09:39:58 +0500

    test(breadcrumbs): remove useless tests

--- src/components/Breadcrumbs/Breadcrumbs.test.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -1,44 +1,6 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import Breadcrumbs from './Breadcrumbs';
-
-it('text check', () => {
-    const options = [
-        { title: 'test 1' },
-        { title: 'test 2' },
-        { title: 'test 3' }
-    ];
-    render(<Breadcrumbs options={options} />);
-    options.forEach((option) => {
-        expect(screen.queryByText(option.title)).toBeTruthy();
-    });
-});
-
-it('link check', () => {
-    const options = [
-        { title: 'test 1', link: '/localhost' },
-        { title: 'test 2' },
-        { title: 'test 3' }
-    ];
-    render(<Breadcrumbs options={options} />);
-    options.forEach((option) => {
-        if (option.link) {
-            expect(screen.getByText('test 1').getAttribute('href')).toBe(
-                '/localhost'
-            );
-        }
-    });
-});
-
-it('icon check', () => {
-    const options = [
-        { title: 'test 1' },
-        { title: 'test 2' },
-        { title: 'test 3' }
-    ];
-    const { container } = render(<Breadcrumbs options={options} />);
-    expect(container.querySelector('#root > div > svg')).toBeDefined();
-});
 
 it('lenght check', () => {
     const options = [
