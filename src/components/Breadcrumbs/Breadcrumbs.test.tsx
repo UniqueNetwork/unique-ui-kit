cfeb776bfc2168dafa2bda0b94510629c31b5425
Pavel Kalachev <pkalachev@usetech.com>
2022-02-09 16:13:38 +0300

    test: Breadcrumbs test name

--- src/components/Breadcrumbs/Breadcrumbs.test.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import Breadcrumbs from './Breadcrumbs';
 
-it('lenght check', () => {
+it('length check', () => {
     const options = [
         { title: 'test 1' },
         { title: 'test 2' },
