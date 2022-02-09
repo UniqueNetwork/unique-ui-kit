3bab22f83249c4e99e407dff9289ee0298f17999
Pavel Kalachev <pkalachev@usetech.com>
2022-02-09 10:19:33 +0300

    test: Icon

--- src/components/Icon/Icon.test.tsx
+++ src/components/Icon/Icon.test.tsx
@@ -1,5 +1,6 @@
-import { fireEvent, getByText, render, screen } from '@testing-library/react';
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import quartz from '../assets/static/quartz.svg';
 import Icon from './Icon';
 
 it('name check', () => {
@@ -18,5 +19,12 @@ it('color check', () => {
     const { container } = render(<Icon name="test" color="#fff" size={16} />);
     expect(container.querySelector('.icon-test')?.getAttribute('fill')).toEqual(
         '#fff'
+    );
+});
+
+it('file prop check', () => {
+    render(<Icon name="test" color="#fff" size={16} file={quartz} />);
+    expect(screen.getByRole('img').getAttribute('src')).toEqual(
+        'test-file-stub'
     );
 });
