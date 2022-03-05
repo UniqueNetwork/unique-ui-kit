230ac00ed251b3cde5374368cd58c77a532a97ab
Pavel Kalachev <pkalachev@usetech.com>
2022-03-05 05:58:11 +0300

    fix: Pagination component perPage fix

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -41,7 +41,7 @@ const Pagination: FC<IPaginationProps> = ({
     const [perPageCount, setPerPageCount] = useState<number>(perPage);
     const prevIndex: number = usePrevious<number>(currentIndex);
     const totalCount = Math.ceil(size / perPageCount);
-    const isOffsetable = totalCount > 3;
+    const isOffsetable = totalCount > 3 && visible < totalCount;
     const hasLeftOffset =
         isOffsetable && currentIndex > (isMobile ? 1 : (visible - 1) / 2);
     const hasRightOffset =
@@ -74,67 +74,65 @@ const Pagination: FC<IPaginationProps> = ({
 
     return (
         <div className="unique-pagination-wrapper">
-            {totalCount > 1 && (
-                <div className="pages-wrapper">
-                    {withIcons && (
+            <div className="pages-wrapper">
+                {withIcons && (
+                    <PageItem
+                        {...(currentIndex !== 0 && {
+                            onClick: () => setCurrentIndex(currentIndex - 1)
+                        })}
+                        className={classNames('page-item', {
+                            disabled: currentIndex === 0
+                        })}
+                    >
+                        <Icon name="carret-left" size={12} />
+                    </PageItem>
+                )}
+                {hasLeftOffset && (
+                    <>
                         <PageItem
-                            {...(currentIndex !== 0 && {
-                                onClick: () => setCurrentIndex(currentIndex - 1)
-                            })}
-                            className={classNames('page-item', {
-                                disabled: currentIndex === 0
-                            })}
-                        >
-                            <Icon name="carret-left" size={12} />
-                        </PageItem>
-                    )}
-                    {hasLeftOffset && (
-                        <>
-                            <PageItem
-                                page={1}
-                                onClick={() => setCurrentIndex(0)}
-                                className="page-item"
-                            />
-                            <div className="page-ellipsis">...</div>
-                        </>
-                    )}
-                    {[...new Array(actualCount)].map((_, index) => {
-                        const offsetIndex = initialIndex + index;
-                        return (
-                            <PageItem
-                                page={offsetIndex + 1}
-                                onClick={() => setCurrentIndex(offsetIndex)}
-                                key={offsetIndex}
-                                className={classNames('page-item', {
-                                    active: offsetIndex === currentIndex
-                                })}
-                            />
-                        );
-                    })}
-                    {hasRightOffset && (
-                        <>
-                            <div className="page-ellipsis">...</div>
-                            <PageItem
-                                page={totalCount}
-                                onClick={() => setCurrentIndex(totalCount - 1)}
-                                className="page-item"
-                            />
-                        </>
-                    )}
-                    {withIcons && (
+                            page={1}
+                            onClick={() => setCurrentIndex(0)}
+                            className="page-item"
+                        />
+                        <div className="page-ellipsis">...</div>
+                    </>
+                )}
+                {[...new Array(actualCount)].map((_, index) => {
+                    const offsetIndex = initialIndex + index;
+                    return (
                         <PageItem
-                            {...(currentIndex !== totalCount - 1 && {
-                                onClick: () => setCurrentIndex(currentIndex + 1)
-                            })}
+                            page={offsetIndex + 1}
+                            onClick={() => setCurrentIndex(offsetIndex)}
+                            key={offsetIndex}
                             className={classNames('page-item', {
-                                disabled: currentIndex === totalCount - 1
+                                active: offsetIndex === currentIndex
                             })}
-                        >
-                            <Icon name="carret-right" size={12} />
-                        </PageItem>
-                    )}
-                </div>
-            )}
+                        />
+                    );
+                })}
+                {hasRightOffset && (
+                    <>
+                        <div className="page-ellipsis">...</div>
+                        <PageItem
+                            page={totalCount}
+                            onClick={() => setCurrentIndex(totalCount - 1)}
+                            className="page-item"
+                        />
+                    </>
+                )}
+                {withIcons && (
+                    <PageItem
+                        {...(currentIndex !== totalCount - 1 && {
+                            onClick: () => setCurrentIndex(currentIndex + 1)
+                        })}
+                        className={classNames('page-item', {
+                            disabled: currentIndex === totalCount - 1
+                        })}
+                    >
+                        <Icon name="carret-right" size={12} />
+                    </PageItem>
+                )}
+            </div>
         </div>
     );
 };
