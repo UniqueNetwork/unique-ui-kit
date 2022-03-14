ac8d6d11c8636cbbb598b2d7de1b43f4e79d85e0
Roman Beganov <rbeganov@usetech.com>
2022-03-14 11:19:24 +0500

    test(table): refactor tests for table

--- src/components/Table/Table.test.tsx
+++ src/components/Table/Table.test.tsx
@@ -2,77 +2,113 @@ import { fireEvent, render } from '@testing-library/react';
 import Link from '../Link/Link';
 import Table from './Table';
 
+const columns = [
+    {
+        title: 'Extrinsic',
+        width: '20%',
+        field: 'ext',
+        render: (data: any) => <Link {...data} />
+    },
+    {
+        title: 'Age',
+        width: '20%',
+        field: 'age'
+    },
+    {
+        title: 'From',
+        width: '20%',
+        field: 'from',
+        render: (data: any) => <Link {...data} />
+    },
+    {
+        title: 'To',
+        width: '20%',
+        field: 'to',
+        render: (data: any) => <Link {...data} />
+    },
+    { title: 'Amount', width: '20%', field: 'amount' }
+];
+
+const data = [
+    {
+        ext: { title: '9666737-0' },
+        age: '11 secs',
+        from: { title: '14KBS...trcQH' },
+        to: { title: 'YqEew...11IJK' },
+        amount: '2 QTZ'
+    },
+    {
+        ext: { title: '9666828-1' },
+        age: '16 secs',
+        from: { title: '23AFx...oPPwR' },
+        to: { title: '77NNm...lLk2L' },
+        amount: '1 QTZ'
+    },
+    {
+        ext: { title: '9666919-2' },
+        age: '5 secs',
+        from: { title: 'D3ws3...xpSm8' },
+        to: { title: '21adF...99Ikkl' },
+        amount: '3 QTZ'
+    }
+];
+
 it('default prop check', () => {
     const { container } = render(
         <Table
             columns={[
+                columns[0],
                 {
-                    title: 'Extrinsic',
-                    width: '20%',
-                    field: 'ext',
+                    ...columns[1],
                     isSortable: true,
-                    iconRight: {
-                        name: 'magnify',
-                        size: 18,
-                        color: 'var(--color-blue-grey-300)'
-                    },
-                    render: (data: any) => <Link {...data} />
-                },
-                { title: 'Age', width: '20%', field: 'age' }
-            ]}
-            data={[
-                {
-                    ext: { title: '9666737-0' },
-                    age: '11 secs ago',
-                    from: { title: '14KBS...trcQH' },
-                    to: { title: '14KBS...trcQH' },
-                    amount: '1 QTZ'
+                    compareFunc: (a: any, b: any) => {
+                        a = Number(a.split(' ')[0]);
+                        b = Number(b.split(' ')[0]);
+                        return a > b ? 1 : a < b ? -1 : 0;
+                    }
                 },
                 {
-                    ext: { title: '9666737-0' },
-                    age: '16 secs ago',
-                    from: { title: '14KBS...trcQH' },
-                    to: { title: '14KBS...trcQH' },
-                    amount: '1 QTZ'
+                    ...columns[2],
+                    iconRight: { name: 'clock', size: 16 }
+                },
+                columns[3],
+                {
+                    ...columns[4],
+                    isSortable: true
                 }
             ]}
+            data={data}
         />
     );
+    const btnSort = container.querySelector(
+        '.table-header-sorter'
+    ) as HTMLDivElement;
+    fireEvent.click(btnSort);
 });
 
 it('with sort prop check', () => {
     const { container } = render(
         <Table
             columns={[
+                columns[0],
                 {
-                    title: 'Extrinsic',
-                    width: '20%',
-                    field: 'ext',
-                    isSortable: true,
-                    render: (data: any) => <Link {...data} />
+                    ...columns[1],
+                    isSortable: true
                 },
-                { title: 'Age', width: '20%', field: 'age' }
-            ]}
-            data={[
+                columns[2],
+                columns[3],
                 {
-                    ext: { title: '9666737-0' },
-                    age: '11 secs ago',
-                    from: { title: '14KBS...trcQH' },
-                    to: { title: '14KBS...trcQH' },
-                    amount: '1 QTZ'
-                },
-                {
-                    ext: { title: '9666737-0' },
-                    age: '16 secs ago',
-                    from: { title: '14KBS...trcQH' },
-                    to: { title: '14KBS...trcQH' },
-                    amount: '1 QTZ'
+                    ...columns[4],
+                    isSortable: true
                 }
             ]}
+            data={data}
             onSort={jest.fn()}
         />
     );
-    const btnSort = container.querySelector('.table-header-sorter');
+    const btnSort = container.querySelector(
+        '.table-header-sorter'
+    ) as HTMLDivElement;
     fireEvent.click(btnSort!);
     fireEvent.click(btnSort!);
 });
