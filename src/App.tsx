9715c72f90f7d16053357ffb30878ca157419791
Pavel Kalachev <pkalachev@usetech.com>
2022-03-28 18:43:27 +0300

    fix: static App example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React, { FC } from 'react';
-import { Button, Layout } from './components';
+import { Select, Layout } from './components';
+
+const optionsKeyValue = [
+    { identity: 'id1', message: 'Option 1' },
+    { identity: 'id2', message: 'Option 2' },
+    { identity: 'id3', message: 'Option 3' },
+    { identity: 'id4', message: 'Option 4' },
+    { identity: 'id5', message: 'Option 5' }
+];
 
 const App: FC = () => (
     <Layout
@@ -12,7 +20,14 @@ const App: FC = () => (
             ]
         }}
     >
-        <Button title="Click me" role="primary" onClick={() => {}} />
+        <Select
+            options={optionsKeyValue}
+            optionKey="identity"
+            optionValue="message"
+            onChange={(option) => {
+                console.log(option);
+            }}
+        />
     </Layout>
 );
 export default App;
