dad53deb4913e86b84adf146834dd0a22ef0e441
Pavel Kalachev <pkalachev@usetech.com>
2022-05-04 12:11:50 +0300

    feat: CollectionLink widget add
    fix: Text component, TokenLink component

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,2 +1,3 @@
 export { default as CollectionCard } from './CollectionCard/CollectionCard';
 export { default as TokenLink } from './TokenLink/TokenLink';
+export { default as CollectionLink } from './CollectionLink/CollectionLink';
