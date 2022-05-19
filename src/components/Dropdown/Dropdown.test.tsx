d56866813de215826596f7d4b7982cd49838c1b3
drgosh <drgosh.m@gmail.com>
2022-05-19 18:23:42 +0300

    fix: Dropdown tests

--- src/components/Dropdown/Dropdown.test.tsx
+++ src/components/Dropdown/Dropdown.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
-import { Button, Dropdown } from "../index";
+import { Button, Dropdown } from '../index';
 
 const mockFunction = jest.fn();
 const options = [
@@ -10,8 +10,8 @@ const options = [
         iconRight: {
             name: 'arrow-up',
             size: 12,
-            color: 'var(--color-blue-grey-300)'
-        }
+            color: 'var(--color-blue-grey-300)',
+        },
     },
     {
         id: 2,
@@ -19,25 +19,22 @@ const options = [
         iconLeft: {
             name: 'arrow-up',
             size: 12,
-            color: 'var(--color-blue-grey-300)'
-        }
+            color: 'var(--color-blue-grey-300)',
+        },
     },
     { id: 3, title: 'test title 3' },
     { id: 4, title: 'test title 4' },
-    { id: 5, title: 'test title 5' }
+    { id: 5, title: 'test title 5' },
 ];
 
 it('it should open dropdown on click by wrapper and close on click outside', () => {
     const { queryByRole, getByTestId } = render(
-        <Dropdown
-            options={options}
-            onChange={mockFunction}
-            id={'dropdown'}
-        >
+        <Dropdown options={options} onChange={mockFunction} id={'dropdown'}>
             <Button title={'Dropdown'} />
         </Dropdown>
     );
     const dropdown = getByTestId('dropdown-wrapper');
+    fireEvent.mouseEnter(dropdown);
     fireEvent.mouseDown(dropdown);
     const dropdownOptions = queryByRole('listbox');
     expect(dropdownOptions).toBeInTheDocument();
@@ -49,16 +46,13 @@ it('it should open dropdown on click by wrapper and close on click outside', () 
 
 it('it should call onChange on click by option', () => {
     const { getByTestId, getByText } = render(
-        <Dropdown
-            options={options}
-            onChange={mockFunction}
-        >
+        <Dropdown options={options} onChange={mockFunction}>
             <Button title={'Dropdown'} />
         </Dropdown>
     );
     const dropdownWrapper = getByTestId('dropdown-wrapper');
     fireEvent.mouseDown(dropdownWrapper);
-    const option = getByText('test title 3')
+    const option = getByText('test title 3');
     fireEvent.click(option);
     expect(mockFunction.mock.calls.length).toBe(1);
     expect(mockFunction).toHaveBeenCalledWith(options[2]);
