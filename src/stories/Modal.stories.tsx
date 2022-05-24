242e7201fb075e581a180b4c8c914b32f39b5e10
drgosh <drgosh.m@gmail.com>
2022-05-24 07:32:23 +0300

    fix: modal with a high height and without scroll inside it;

--- src/stories/Modal.stories.tsx
+++ src/stories/Modal.stories.tsx
@@ -5,7 +5,7 @@ import { Button, Modal, Heading, Text, Tabs } from '../components';
 export default {
     title: 'Components/Modal',
     component: Modal,
-    subcomponents: { Button, Heading, Text, Tabs }
+    subcomponents: { Button, Heading, Text, Tabs },
 } as ComponentMeta<typeof Modal>;
 
 const Template: ComponentStory<typeof Modal> = (args) => {
@@ -41,17 +41,12 @@ const Template: ComponentStory<typeof Modal> = (args) => {
 export const Default = Template.bind({});
 
 Default.args = {
-    title: 'Modal title',
-    isClosable: true
+    isClosable: true,
 };
 
 Default.storyName = 'Default';
 
 export const DefaultNonClosable = Template.bind({});
-
-DefaultNonClosable.args = {
-    title: 'Modal title'
-};
 
 DefaultNonClosable.storyName = 'Default w/o closure';
 
@@ -99,8 +94,7 @@ const TemplateTabs: ComponentStory<typeof Modal> = (args) => {
 export const DefaultTabs = TemplateTabs.bind({});
 
 DefaultTabs.args = {
-    title: 'Modal title with Tabs',
-    isClosable: true
+    isClosable: true,
 };
 
 DefaultTabs.storyName = 'Default w/ Tabs';
@@ -135,7 +129,7 @@ const TemplateButtons: ComponentStory<typeof Modal> = (args) => {
                         display: 'flex',
                         justifyContent: 'flex-end',
                         gap: 20,
-                        marginTop: 20
+                        marginTop: 20,
                     }}
                 >
                     <Button
@@ -156,8 +150,111 @@ const TemplateButtons: ComponentStory<typeof Modal> = (args) => {
 export const DefaultButtons = TemplateButtons.bind({});
 
 DefaultButtons.args = {
-    title: 'Modal title with Tabs',
-    isClosable: true
+    isClosable: true,
 };
 
 DefaultButtons.storyName = 'Default w/ Buttons';
+
+const TemplateHighHeight: ComponentStory<typeof Modal> = (args) => {
+    const [isVisible, setVisible] = useState<boolean>(true);
+    return (
+        <>
+            <Button
+                title="Open modal"
+                role="primary"
+                onClick={() => setVisible(true)}
+            />
+            <Modal
+                {...args}
+                isVisible={isVisible}
+                onClose={() => setVisible(false)}
+            >
+                <Heading size="2">Modal title</Heading>
+                <Text>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Tellus elementum sagittis vitae et leo duis ut.
+                    Lobortis elementum nibh tellus molestie nunc. Facilisis
+                    mauris sit amet massa vitae. Et magnis dis parturient montes
+                    nascetur ridiculus mus. Natoque penatibus et magnis dis.
+                    Curabitur gravida arcu ac tortor dignissim convallis aenean.
+                    Tristique et egestas quis ipsum. Leo in vitae turpis massa
+                    sed elementum. Sed augue lacus viverra vitae congue eu
+                    consequat ac felis. Quis imperdiet massa tincidunt nunc
+                    pulvinar sapien et. Sit amet facilisis magna etiam tempor
+                    orci eu. Ut pharetra sit amet aliquam id. Erat pellentesque
+                    adipiscing commodo elit at imperdiet. Congue nisi vitae
+                    suscipit tellus mauris a diam maecenas sed. Vel quam
+                    elementum pulvinar etiam non quam lacus suspendisse. Est
+                    pellentesque elit ullamcorper dignissim cras tincidunt. In
+                    est ante in nibh. Et malesuada fames ac turpis egestas
+                    integer eget. Tristique senectus et netus et.
+                </Text>
+                <Text>
+                    Nibh sit amet commodo nulla facilisi nullam vehicula ipsum.
+                    Volutpat diam ut venenatis tellus in metus vulputate eu
+                    scelerisque. Pharetra et ultrices neque ornare aenean
+                    euismod elementum nisi. Urna molestie at elementum eu
+                    facilisis. Viverra nam libero justo laoreet sit amet cursus.
+                    Porttitor massa id neque aliquam vestibulum. Etiam sit amet
+                    nisl purus in mollis. Sed elementum tempus egestas sed sed
+                    risus pretium quam vulputate. Sociis natoque penatibus et
+                    magnis dis parturient montes. Quam elementum pulvinar etiam
+                    non quam lacus suspendisse faucibus. Purus semper eget duis
+                    at tellus at urna. At risus viverra adipiscing at. Ut
+                    consequat semper viverra nam libero justo laoreet. Purus non
+                    enim praesent elementum facilisis leo vel fringilla. Dolor
+                    sit amet consectetur adipiscing elit. Tortor dignissim
+                    convallis aenean et tortor at. Lobortis feugiat vivamus at
+                    augue eget. Egestas egestas fringilla phasellus faucibus
+                    scelerisque eleifend donec pretium vulputate.
+                </Text>
+                <Text>
+                    Diam phasellus vestibulum lorem sed risus ultricies. Morbi
+                    tristique senectus et netus et. Nullam ac tortor vitae purus
+                    faucibus ornare suspendisse sed nisi. Lobortis feugiat
+                    vivamus at augue eget arcu dictum varius. Suspendisse in est
+                    ante in nibh mauris cursus mattis. Amet facilisis magna
+                    etiam tempor orci eu. At urna condimentum mattis
+                    pellentesque id. Varius duis at consectetur lorem donec.
+                    Vehicula ipsum a arcu cursus vitae congue. Fermentum iaculis
+                    eu non diam phasellus vestibulum lorem. Orci porta non
+                    pulvinar neque laoreet suspendisse interdum consectetur.
+                    Tellus in hac habitasse platea. Tincidunt arcu non sodales
+                    neque sodales. Egestas fringilla phasellus faucibus
+                    scelerisque eleifend donec. Felis donec et odio pellentesque
+                    diam volutpat commodo sed egestas. Viverra suspendisse
+                    potenti nullam ac tortor vitae purus faucibus ornare. Augue
+                    mauris augue neque gravida in.
+                </Text>
+                <Text>
+                    Egestas egestas fringilla phasellus faucibus scelerisque
+                    eleifend donec pretium vulputate. Morbi leo urna molestie
+                    at. Amet facilisis magna etiam tempor orci eu lobortis
+                    elementum nibh. Malesuada fames ac turpis egestas integer
+                    eget aliquet. Enim nunc faucibus a pellentesque sit amet
+                    porttitor. Interdum posuere lorem ipsum dolor sit. Fames ac
+                    turpis egestas sed tempus. Sapien nec sagittis aliquam
+                    malesuada bibendum arcu. Nunc eget lorem dolor sed viverra
+                    ipsum nunc aliquet bibendum. Quam adipiscing vitae proin
+                    sagittis nisl rhoncus mattis rhoncus. Et tortor at risus
+                    viverra adipiscing at. Faucibus turpis in eu mi bibendum
+                    neque. Quisque egestas diam in arcu cursus euismod quis
+                    viverra nibh. A condimentum vitae sapien pellentesque
+                    habitant morbi tristique senectus. Morbi blandit cursus
+                    risus at ultrices mi tempus imperdiet. Amet est placerat in
+                    egestas erat. Diam donec adipiscing tristique risus nec
+                    feugiat in fermentum.
+                </Text>
+            </Modal>
+        </>
+    );
+};
+
+export const DefaultHighHeight = TemplateHighHeight.bind({});
+
+DefaultHighHeight.args = {
+    isClosable: true,
+};
+
+DefaultHighHeight.storyName = 'Default w/ high height';
