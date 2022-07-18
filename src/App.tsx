c7fd4d8621dbb47c1db8ebaf732bd6ba0e00eb9a
pavlyxar <pavlyxar567@inbox.ru>
2022-07-18 14:35:52 +0300

    feat: add empty image for Avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react';
-import { Select, Layout } from './components';
+import { Select, Layout, Icon, Avatar } from './components';
 
 const optionsKeyValue = [
     { identity: 'id1', message: 'Option 1' },
     { identity: 'id2', message: 'Option 2' },
     { identity: 'id3', message: 'Option 3' },
     { identity: 'id4', message: 'Option 4' },
-    { identity: 'id5', message: 'Option 5' }
+    { identity: 'id5', message: 'Option 5' },
 ];
 
 const App: FC = () => (
@@ -16,18 +16,11 @@ const App: FC = () => (
             options: [
                 { title: 'Home', link: '#' },
                 { title: 'Section', link: '#' },
-                { title: 'Current', link: '#' }
-            ]
+                { title: 'Current', link: '#' },
+            ],
         }}
     >
-        <Select
-            options={optionsKeyValue}
-            optionKey="identity"
-            optionValue="message"
-            onChange={(option) => {
-                console.log(option);
-            }}
-        />
+        <Avatar size={24} type="circle" />
     </Layout>
 );
 export default App;
--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -3,18 +3,20 @@
  */
 
 import './Avatar.scss';
+import EmptyCircle from '../../assets/static/empty-circle.svg';
+import EmptySquare from '../../assets/static/empty-square.svg';
 
 export interface IAvatarProps {
-    src: string;
+    src?: string;
     size?: number;
     type?: 'circle' | 'square';
 }
 
 export const Avatar = ({ src, size = 38, type = 'square' }: IAvatarProps) => (
     <img
-        className={`unique-avatar ${type}`}
-        src={src}
         width={size}
         height={size}
+        className={`unique-avatar ${type}`}
+        src={src || (type === 'square' && EmptySquare) || EmptyCircle}
     />
 );
