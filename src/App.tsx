e1810d9b78f1d7299a394ec9e3a586c7c6825933
pavlyxar <pavlyxar567@inbox.ru>
2022-07-18 14:37:40 +0300

    fix: return app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react';
-import { Select, Layout, Icon, Avatar } from './components';
+import { Select, Layout } from './components';
 
 const optionsKeyValue = [
     { identity: 'id1', message: 'Option 1' },
     { identity: 'id2', message: 'Option 2' },
     { identity: 'id3', message: 'Option 3' },
     { identity: 'id4', message: 'Option 4' },
-    { identity: 'id5', message: 'Option 5' },
+    { identity: 'id5', message: 'Option 5' }
 ];
 
 const App: FC = () => (
@@ -16,11 +16,18 @@ const App: FC = () => (
             options: [
                 { title: 'Home', link: '#' },
                 { title: 'Section', link: '#' },
-                { title: 'Current', link: '#' },
-            ],
+                { title: 'Current', link: '#' }
+            ]
         }}
     >
-        <Avatar size={24} type="circle" />
+        <Select
+            options={optionsKeyValue}
+            optionKey="identity"
+            optionValue="message"
+            onChange={(option) => {
+                console.log(option);
+            }}
+        />
     </Layout>
 );
 export default App;
