d221a6a872a384c821b7cbe3104cdab9da283100
Andrey O <aovsyannikov@usetech.com>
2022-07-18 13:31:24 +0400

    [UI-135] - TokenLink widget. - Change title color; - add click handle for use react routing.

--- src/widgets/TokenLink/TokenLink.tsx
+++ src/widgets/TokenLink/TokenLink.tsx
@@ -2,6 +2,7 @@
  * @author Pavel Kalachev <pkalachev@usetech.com>
  */
 
+import React from 'react';
 import { Avatar, Text } from '../../components';
 import './TokenLink.scss';
 
@@ -12,16 +13,29 @@ export interface ITokenLinkProps {
         title: string;
         href: string;
     };
+    onNavigation?: () => void;
 }
 
-export const TokenLink = ({ image, title, link }: ITokenLinkProps) => (
-    <a href={link.href} className="unique-token-link">
-        <Avatar src={image} size={326} type="square" />
-        <Text size="l" appearance="block">
-            {title}
-        </Text>
-        <Text size="s" color="primary-500" appearance="block">
-            {link.title}
-        </Text>
-    </a>
-);
+export const TokenLink = ({ image, title, link, onNavigation }: ITokenLinkProps) => {
+    const handleClick = (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+        onNavigation?.();
+    }
+
+    return (
+        <a href={link.href} className="unique-token-link" onClick={handleClick}>
+            <Avatar src={image} size={326} type="square" />
+            <Text size="l" appearance="block">
+                {title}
+            </Text>
+            <Text
+                appearance="block"
+                className="unique-token-link__description"
+                color="grey-500"
+                size="s"
+            >
+                {link.title}
+            </Text>
+        </a>
+    );
+};
