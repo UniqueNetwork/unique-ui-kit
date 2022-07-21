af51dd58078769a95ea78db3a54d09a283c3861d
Kozlov Sergey <skozlov@usetech.ru>
2022-07-21 17:59:23 +0300

    feat: [UI-141] - For Textarea, Checkbox, RadioGroup elements need to pass ref

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -5,7 +5,7 @@
 import classNames from 'classnames';
 import './Checkbox.scss';
 import { Icon, IconProps } from '..';
-import { ReactNode } from 'react';
+import { ReactNode, forwardRef, LegacyRef } from 'react';
 import { ComponentProps } from '../../types';
 
 export interface CheckboxProps extends Omit<ComponentProps, 'onChange'> {
@@ -18,56 +18,62 @@ export interface CheckboxProps extends Omit<ComponentProps, 'onChange'> {
     iconLeft?: IconProps;
 }
 
-export const Checkbox = ({
-    id,
-    name,
-    checked,
-    label,
-    disabled,
-    size = 's',
-    onChange,
-    iconRight,
-    iconLeft,
-}: CheckboxProps) => {
-    const icon = iconLeft || iconRight;
-    return (
-        <div
-            className={classNames(
-                'unique-checkbox-wrapper',
-                `checkbox-size-${size}`,
-                { disabled }
-            )}
-            {...(!disabled && {
-                onClick: () => onChange(!checked),
-            })}
-        >
-            <input
-                type="checkbox"
-                name={name}
-                id={id}
-                className="checkbox"
-                checked={checked}
-                onChange={(e) => e.preventDefault()}
-            />
-            <span className={classNames('checkmark', { checked })}>
-                {checked && (
-                    <Icon
-                        name="checked"
-                        color="#fff"
-                        size={size === 's' ? 16 : 18}
-                    />
+export const Checkbox = forwardRef(
+    (
+        {
+            id,
+            name,
+            checked,
+            label,
+            disabled,
+            size = 's',
+            onChange,
+            iconRight,
+            iconLeft,
+        }: CheckboxProps,
+        ref: LegacyRef<HTMLInputElement>
+    ) => {
+        const icon = iconLeft || iconRight;
+        return (
+            <div
+                className={classNames(
+                    'unique-checkbox-wrapper',
+                    `checkbox-size-${size}`,
+                    { disabled }
                 )}
-            </span>
-
-            <label
-                className={classNames('checkbox-label', {
-                    'icon-left': iconLeft,
-                    'icon-right': iconRight,
+                {...(!disabled && {
+                    onClick: () => onChange(!checked),
                 })}
             >
-                {icon && <Icon {...icon} />}
-                <span>{label}</span>
-            </label>
-        </div>
-    );
-};
+                <input
+                    type="checkbox"
+                    name={name}
+                    id={id}
+                    className="checkbox"
+                    checked={checked}
+                    ref={ref}
+                    onChange={(e) => e.preventDefault()}
+                />
+                <span className={classNames('checkmark', { checked })}>
+                    {checked && (
+                        <Icon
+                            name="checked"
+                            color="#fff"
+                            size={size === 's' ? 16 : 18}
+                        />
+                    )}
+                </span>
+
+                <label
+                    className={classNames('checkbox-label', {
+                        'icon-left': iconLeft,
+                        'icon-right': iconRight,
+                    })}
+                >
+                    {icon && <Icon {...icon} />}
+                    <span>{label}</span>
+                </label>
+            </div>
+        );
+    }
+);
--- src/components/RadioGroup/RadioGroup.tsx
+++ src/components/RadioGroup/RadioGroup.tsx
@@ -4,7 +4,7 @@
 
 import classNames from 'classnames';
 import './RadioGroup.scss';
-import { useState } from 'react';
+import { forwardRef, LegacyRef, useState } from 'react';
 
 export type RadioOptionValueType = {
     value: string;
@@ -21,65 +21,71 @@ export interface RadioGroupProps {
     defaultValue?: string | null;
 }
 
-export const RadioGroup = ({
-    options,
-    onChange,
-    value: userValue,
-    size = 's',
-    align = 'vertical',
-    defaultValue = null,
-}: RadioGroupProps) => {
-    const [defaultRadioValue, setDefaultRadioValue] = useState(() => {
-        if (!defaultValue && !userValue) {
-            return options[0]?.value ?? null;
-        }
-        return defaultValue;
-    });
+export const RadioGroup = forwardRef(
+    (
+        {
+            options,
+            onChange,
+            value: userValue,
+            size = 's',
+            align = 'vertical',
+            defaultValue = null,
+        }: RadioGroupProps,
+        ref: LegacyRef<HTMLInputElement>
+    ) => {
+        const [defaultRadioValue, setDefaultRadioValue] = useState(() => {
+            if (!defaultValue && !userValue) {
+                return options[0]?.value ?? null;
+            }
+            return defaultValue;
+        });
 
-    const handleChange = (radio: RadioOptionValueType) => {
-        if (defaultRadioValue) {
-            setDefaultRadioValue(radio.value);
-        }
-        onChange?.(radio);
-    };
+        const handleChange = (radio: RadioOptionValueType) => {
+            if (defaultRadioValue) {
+                setDefaultRadioValue(radio.value);
+            }
+            onChange?.(radio);
+        };
 
-    return (
-        <div className={`unique-radio-group-wrapper ${align}`}>
-            {options.map((radio) => {
-                const { disabled, value: radioValue, label } = radio;
-                const value = defaultRadioValue ?? userValue;
+        return (
+            <div className={`unique-radio-group-wrapper ${align}`}>
+                {options.map((radio) => {
+                    const { disabled, value: radioValue, label } = radio;
+                    const value = defaultRadioValue ?? userValue;
 
-                const isChecked = radioValue === value;
-                return (
-                    <div
-                        className={classNames(
-                            'unique-radio-item',
-                            `radio-size-${size}`,
-                            {
-                                disabled,
-                            }
-                        )}
-                        key={radioValue}
-                    >
-                        <label htmlFor={radioValue}>
-                            <input
-                                type="radio"
-                                id={radioValue}
-                                checked={isChecked}
-                                onChange={() => handleChange(radio)}
-                                hidden={true}
-                                disabled={disabled}
-                            />
-                            <span
-                                className={classNames('inner', {
-                                    checked: isChecked,
-                                })}
-                            />
-                            <span className={'label'}>{label}</span>
-                        </label>
-                    </div>
-                );
-            })}
-        </div>
-    );
-};
+                    const isChecked = radioValue === value;
+                    return (
+                        <div
+                            className={classNames(
+                                'unique-radio-item',
+                                `radio-size-${size}`,
+                                {
+                                    disabled,
+                                }
+                            )}
+                            key={radioValue}
+                        >
+                            <label htmlFor={radioValue}>
+                                <input
+                                    type="radio"
+                                    id={radioValue}
+                                    checked={isChecked}
+                                    onChange={() => handleChange(radio)}
+                                    hidden={true}
+                                    disabled={disabled}
+                                    ref={ref}
+                                />
+                                <span
+                                    className={classNames('inner', {
+                                        checked: isChecked,
+                                    })}
+                                />
+                                <span className={'label'}>{label}</span>
+                            </label>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
+);
