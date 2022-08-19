d1a2afb4bfb06fb00e7b0b74fc020677720038b1
drgosh <drgosh.m@gmail.com>
2022-08-19 12:25:53 +0300

    fix: memoization of context value for Notifications

--- src/components/Notifications/hook/useNotifications.ts
+++ src/components/Notifications/hook/useNotifications.ts
@@ -1,17 +1,20 @@
 import { NotificationsContext } from '../Notifications';
-import { ReactNode, useContext } from 'react';
+import { ReactNode, useContext, useMemo } from 'react';
 import { IconProps } from '../../Icon';
 
 export const useNotifications = () => {
     const { push, clearAll } = useContext(NotificationsContext);
 
-    return {
-        info: (content: ReactNode, icon?: IconProps) =>
-            push({ content, severity: 'info', icon }),
-        warning: (content: ReactNode, icon?: IconProps) =>
-            push({ content, severity: 'warning', icon }),
-        error: (content: ReactNode, icon?: IconProps) =>
-            push({ content, severity: 'error', icon }),
-        clearAll,
-    };
+    return useMemo(
+        () => ({
+            info: (content: ReactNode, icon?: IconProps) =>
+                push({ content, severity: 'info', icon }),
+            warning: (content: ReactNode, icon?: IconProps) =>
+                push({ content, severity: 'warning', icon }),
+            error: (content: ReactNode, icon?: IconProps) =>
+                push({ content, severity: 'error', icon }),
+            clearAll,
+        }),
+        [push, clearAll]
+    );
 };
